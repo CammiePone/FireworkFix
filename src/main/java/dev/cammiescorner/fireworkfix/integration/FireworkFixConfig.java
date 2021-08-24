277085136f74207a1d26edbd6815a10232c171a6
CammiePone <inazuma261@gmail.com>
2021-08-24 13:05:48 -0400

    Added rocket jumping :)

--- src/main/java/dev/cammiescorner/fireworkfix/integration/FireworkFixConfig.java
+++ src/main/java/dev/cammiescorner/fireworkfix/integration/FireworkFixConfig.java
@@ -10,9 +10,15 @@ public class FireworkFixConfig implements ConfigData {
 	@Comment("Base damage of Firework Rockets.")
 	public float baseDamage = 4F;
 
+	@Comment("Multiplier for Rocket Jump Velocity")
+	public double rocketJumpMultiplier = 1F;
+
 	@Comment("Should item display damage of Firework Rockets?")
 	public boolean showTooltip = true;
 
-	@Comment("Should only affect crossbows?")
+	@Comment("Should damage scaling only affect crossbows?")
 	public boolean onlyAffectCrossbows = true;
+
+	@Comment("Should players be able to Rocket Jump?")
+	public boolean allowRocketJumping = true;
 }
--- src/main/java/dev/cammiescorner/fireworkfix/mixin/FireworkRocketEntityMixin.java
+++ src/main/java/dev/cammiescorner/fireworkfix/mixin/FireworkRocketEntityMixin.java
@@ -1,25 +1,18 @@
 package dev.cammiescorner.fireworkfix.mixin;
 
 import dev.cammiescorner.fireworkfix.FireworkFix;
-import net.minecraft.enchantment.ProtectionEnchantment;
 import net.minecraft.entity.*;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.projectile.FireworkRocketEntity;
-import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtList;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import net.minecraft.world.explosion.Explosion;
-import net.minecraft.world.explosion.ExplosionBehavior;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -34,6 +27,17 @@ public abstract class FireworkRocketEntityMixin extends Entity implements Flying
 
 	public FireworkRocketEntityMixin(EntityType<?> type, World world) {
 		super(type, world);
+	}
+
+	@Inject(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", ordinal = 1), locals = LocalCapture.CAPTURE_FAILSOFT)
+	public void explode(CallbackInfo info, float damage, double d, Vec3d pos, List list, Iterator iterator, LivingEntity entity) {
+		if(FireworkFix.config.allowRocketJumping) {
+			Vec3d vec3d = new Vec3d(entity.getX(), entity.getEyeY(), entity.getZ());
+			double inverseDistance = getPos().distanceTo(vec3d) != 0 ? (1 / getPos().distanceTo(vec3d)) * 1.5 : 1;
+
+			entity.setVelocity(getVelocity().multiply(-inverseDistance * FireworkFix.config.rocketJumpMultiplier));
+			entity.velocityModified = true;
+		}
 	}
 
 	@ModifyArg(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", ordinal = 0))
