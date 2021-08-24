fc2c3130e3d246f2221c83a185c3a934c2cb4e85
CammiePone <inazuma261@gmail.com>
2021-08-24 11:29:16 -0400

    aaaaaaaaaaa

--- src/main/java/dev/cammiescorner/fireworkfix/mixin/FireworkRocketEntityMixin.java
+++ src/main/java/dev/cammiescorner/fireworkfix/mixin/FireworkRocketEntityMixin.java
@@ -1,16 +1,18 @@
 package dev.cammiescorner.fireworkfix.mixin;
 
 import dev.cammiescorner.fireworkfix.FireworkFix;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.FlyingItemEntity;
+import net.minecraft.enchantment.ProtectionEnchantment;
+import net.minecraft.entity.*;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.projectile.FireworkRocketEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+import net.minecraft.world.explosion.ExplosionBehavior;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,6 +22,9 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Iterator;
+import java.util.List;
 
 @Mixin(FireworkRocketEntity.class)
 public abstract class FireworkRocketEntityMixin extends Entity implements FlyingItemEntity {
@@ -31,17 +36,17 @@ public abstract class FireworkRocketEntityMixin extends Entity implements Flying
 		super(type, world);
 	}
 
-	@ModifyArg(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", ordinal = 1))
-	public float crossbowDamage(float amount) {
-		if(hasExplosionEffects())
+	@ModifyArg(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", ordinal = 0))
+	public float selfDamage(float amount) {
+		if(!FireworkFix.config.onlyAffectCrossbows && hasExplosionEffects())
 			return FireworkFix.config.baseDamage * dataTracker.get(ITEM).getSubNbt("Fireworks").getList("Explosions", 10).size();
 
 		return amount;
 	}
 
-	@ModifyArg(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", ordinal = 0))
-	public float selfDamage(float amount) {
-		if(!FireworkFix.config.onlyAffectCrossbows && hasExplosionEffects())
+	@ModifyArg(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", ordinal = 1))
+	public float crossbowDamage(float amount) {
+		if(hasExplosionEffects())
 			return FireworkFix.config.baseDamage * dataTracker.get(ITEM).getSubNbt("Fireworks").getList("Explosions", 10).size();
 
 		return amount;
