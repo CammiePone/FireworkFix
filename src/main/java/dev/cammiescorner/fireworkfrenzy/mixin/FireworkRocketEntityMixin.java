83c33282e960f3bab9550d8272987341d57167f4
CammiePone <inazuma261@gmail.com>
2022-02-24 17:40:14 -0500

    some weird jank still exists what

--- src/main/java/dev/cammiescorner/fireworkfrenzy/mixin/FireworkRocketEntityMixin.java
+++ src/main/java/dev/cammiescorner/fireworkfrenzy/mixin/FireworkRocketEntityMixin.java
@@ -37,51 +37,46 @@ public abstract class FireworkRocketEntityMixin extends ProjectileEntity impleme
 	@Shadow protected abstract boolean hasExplosionEffects();
 	@Shadow public abstract ItemStack getStack();
 
-	@Unique public LivingEntity splashTarget;
 	@Unique public LivingEntity directTarget;
-	@Unique public double damageMultiplier;
 
 	public FireworkRocketEntityMixin(EntityType<? extends ProjectileEntity> type, World world) { super(type, world); }
 
-	@Inject(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", ordinal = 1),  locals = LocalCapture.CAPTURE_FAILSOFT)
-	public void fireworkfrenzy$explodePreDamage(CallbackInfo info, float damage, ItemStack stack, NbtCompound tag, NbtList nbtList, double d, Vec3d vec, List<LivingEntity> list, Iterator<LivingEntity> iterator, LivingEntity entity) {
-		splashTarget = entity;
-	}
-
 	@Inject(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", ordinal = 1), locals = LocalCapture.CAPTURE_FAILSOFT)
-	public void fireworkfrenzy$explodePostDamage(CallbackInfo info, float damage, ItemStack stack, NbtCompound tag, NbtList nbtList, double d, Vec3d vec, List<LivingEntity> list, Iterator<LivingEntity> iterator, LivingEntity entity) {
+	public void fireworkfrenzy$explodePostDamage(CallbackInfo info, float damage, ItemStack stack, NbtCompound tag, NbtList nbtList, double d, Vec3d vec, List<LivingEntity> list, Iterator<LivingEntity> iterator, LivingEntity target) {
 		NbtCompound subNbt = dataTracker.get(ITEM).getSubNbt("Fireworks");
 
 		if(FireworkFrenzy.config.allowRocketJumping && hasExplosionEffects() && subNbt != null) {
 			Box box = getBoundingBox().expand(d);
 			float radius = (float) (box.getXLength() / 2);
 			double multiplier = (subNbt.getList("Explosions", NbtElement.COMPOUND_TYPE).size() / 4.5D) * FireworkFrenzy.config.rocketJumpMultiplier;
+			DamageSource source = DamageSource.firework((FireworkRocketEntity) (Object) this, getOwner());
 
-			for(LivingEntity target : list) {
-				if(!target.blockedByShield(DamageSource.firework((FireworkRocketEntity) (Object) this, getOwner()))) {
-					EntityHitResult hitResult = ProjectileUtil.raycast(this, getPos(), target.getPos(), box, hit -> !hit.isSpectator(), box.getAverageSideLength() * 2);
-					Vec3d targetPos;
-					Vec3d velocityDirection;
+			if(!target.blockedByShield(source)) {
+				EntityHitResult hitResult = ProjectileUtil.raycast(this, getPos(), target.getPos().add(0, target.getHeight() * 0.5, 0), box, hit -> !hit.isSpectator(), 20);
 
-					if(hitResult != null) {
-						targetPos = hitResult.getPos();
-						velocityDirection = new Vec3d(targetPos.getX() - getX(), targetPos.getY() - getY(), targetPos.getZ() - getZ());
-						damageMultiplier = Math.max(0, 1 - (velocityDirection.length() / radius));
-					}
+				if(hitResult != null) {
+					Vec3d direction = hitResult.getPos().subtract(getPos());
+					float damageMultiplier = (float) Math.max(0, 1 - (direction.length() / radius));
+					float fireworkDamage = FireworkFrenzy.config.baseDamage * subNbt.getList("Explosions", NbtElement.COMPOUND_TYPE).size();
 
-					targetPos = new Vec3d(target.getX(), target.getY() + (target.getHeight() / 2), target.getZ());
-					velocityDirection = new Vec3d(target.getX() - getX(), targetPos.getY() - getY(), target.getZ() - getZ());
-					double inverseDistance = Math.max(0, 1 - (velocityDirection.length() / radius));
-
-					target.knockbackVelocity = 0F;
-					target.setVelocity(target.getVelocity().getX(), Math.min(1D, Math.abs(target.getVelocity().getY())), target.getVelocity().getZ());
-					target.setVelocity(target.getVelocity().add(velocityDirection).multiply(inverseDistance * (target == getOwner() ? multiplier : multiplier * FireworkFrenzy.config.otherEntityKnockback)));
-					target.velocityModified = true;
+					if(target == directTarget)
+						target.damage(source, fireworkDamage);
+					else
+						target.damage(source, fireworkDamage * damageMultiplier);
 				}
+
+				Vec3d targetPos = target.getPos().add(0, target.getHeight() * 0.5, 0);
+				Vec3d direction = targetPos.subtract(getPos());
+				double inverseDistance = Math.max(0, 1 - (direction.length() / radius));
+
+				target.knockbackVelocity = 0F;
+				target.setVelocity(target.getVelocity().getX(), Math.min(1D, Math.abs(target.getVelocity().getY())), target.getVelocity().getZ());
+				target.setVelocity(target.getVelocity().add(direction).multiply(inverseDistance * (target == getOwner() ? multiplier : multiplier * FireworkFrenzy.config.otherEntityKnockback)));
+				target.velocityModified = true;
 			}
 		}
 
-		if(entity instanceof BlastJumper jumper) {
+		if(target instanceof BlastJumper jumper) {
 			jumper.setTimeOnGround(0);
 			jumper.setBlastJumping(true);
 		}
@@ -95,27 +90,11 @@ public abstract class FireworkRocketEntityMixin extends ProjectileEntity impleme
 
 	@ModifyArg(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", ordinal = 0))
 	public float fireworkfrenzy$selfDamage(DamageSource source, float amount) {
-		NbtCompound subNbt = dataTracker.get(ITEM).getSubNbt("Fireworks");
-
-		if(hasExplosionEffects() && subNbt != null)
-			return (float) ((FireworkFrenzy.config.baseDamage * subNbt.getList("Explosions", NbtElement.COMPOUND_TYPE).size()) * damageMultiplier);
-
-		return amount;
+		return 0;
 	}
 
 	@ModifyArg(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", ordinal = 1))
 	public float fireworkfrenzy$crossbowDamage(DamageSource source, float amount) {
-		NbtCompound subNbt = dataTracker.get(ITEM).getSubNbt("Fireworks");
-
-		if(hasExplosionEffects() && subNbt != null) {
-			float damage = FireworkFrenzy.config.baseDamage * subNbt.getList("Explosions", NbtElement.COMPOUND_TYPE).size();
-
-			if(splashTarget == directTarget)
-				return damage;
-
-			return (float) (damage * damageMultiplier);
-		}
-
-		return amount;
+		return 0;
 	}
 }
